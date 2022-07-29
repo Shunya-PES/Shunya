da789d98bb38ba30d3553f96064672500a73e1aa
Sarthak S Kumar <sskworld9742@gmail.com>
2022-07-29 11:27:25 +0530

    Navbar Changes for Responsive Design

--- main.js
+++ main.js
@@ -33,7 +33,7 @@ if (screen.width >= 580 && screen.width < 700) {
     VANTA.NET({
         el: "#vantajs",
         mouseControls: true,
-        touchControls: false,
+        touchControls: true,
         minHeight: 100.00,
         minWidth: 100.00,
         scale: 1.00,
@@ -45,19 +45,6 @@ if (screen.width >= 580 && screen.width < 700) {
         spacing: 30.00
     })
 }
-window.onscroll = function () {
-    if (window.scrollY >= 500 || window.pageYOffset >= 500) {
-        document.getElementsByTagName("header")[0].style.backgroundColor = "#001322"
-        document.getElementsByTagName("header")[0].style.transition = "0.4s"
-        document.querySelector(".nav-logo").style.height = "100px"
-        document.querySelector(".nav-logo").style.transition = "0.4s"
-    } else {
-        document.getElementsByTagName("header")[0].style.backgroundColor = "transparent"
-        document.getElementsByTagName("header")[0].style.transition = "0.4s"
-        document.querySelector(".nav-logo").style.height = "170px"
-        document.querySelector(".nav-logo").style.transition = "0.4s"
-    }
-};
 function openNav() {
     document.getElementById("mySidenav").style.width = "100px";
 }
