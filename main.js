fe1b05934d0c44f46774b1cfe84f494ead6a6e59
Sarthak S Kumar <sskworld9742@gmail.com>
2022-07-29 10:57:09 +0530

    Minor Changes

--- main.js
+++ main.js
@@ -12,6 +12,39 @@ VANTA.NET({
     maxDistance: 35.00,
     spacing: 20.00
 })
+
+if (screen.width >= 350 && screen.width < 580) {
+    VANTA.NET({
+        el: "#vantajs",
+        mouseControls: true,
+        touchControls: true,
+        minHeight: 100.00,
+        minWidth: 100.00,
+        scale: 1.00,
+        scaleMobile: 1.00,
+        color: 0x0382b8,
+        backgroundColor: 0x001322,
+        points: 10.00,
+        maxDistance: 35.00,
+        spacing: 30.00
+    })
+}
+if (screen.width >= 580 && screen.width < 700) {
+    VANTA.NET({
+        el: "#vantajs",
+        mouseControls: true,
+        touchControls: false,
+        minHeight: 100.00,
+        minWidth: 100.00,
+        scale: 1.00,
+        scaleMobile: 1.00,
+        color: 0x0382b8,
+        backgroundColor: 0x001322,
+        points: 10.00,
+        maxDistance: 35.00,
+        spacing: 30.00
+    })
+}
 window.onscroll = function () {
     if (window.scrollY >= 500 || window.pageYOffset >= 500) {
         document.getElementsByTagName("header")[0].style.backgroundColor = "#001322"
